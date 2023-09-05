cad10019798d3c732c5395d57501f5d416685505
iepie122 <90779175+iepie122@users.noreply.github.com>
2023-09-05 13:34:23 +0200

    Add files via upload
    framework added

---
+++ Patient.java
@@ -0,0 +1,48 @@
+import java.time.LocalDate;
+
+class Patient {
+   static final int RETURN      = 0;
+   static final int SURNAME     = 1;
+   static final int FIRSTNAME   = 2;
+   static final int DATEOFBIRTH = 3;
+
+   int       id;
+   String    surname;
+   String    firstName;
+   LocalDate dateOfBirth;
+
+    /**
+     * Constructor
+     */
+    Patient(int id, String surname, String firstName, LocalDate dateOfBirth) {
+        this.id = id;
+        this.surname = surname;
+        this.firstName = firstName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    String getSurname() {
+        return surname;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Display patient data.
+     */
+    void viewData() {
+        System.out.format("===== Patient id=%d ==============================\n", id);
+        System.out.format("%-17s %s\n", "Surname:", surname);
+        System.out.format("%-17s %s\n", "firstName:", firstName);
+        System.out.format("%-17s %s\n", "Date of birth:", dateOfBirth);
+    }
+
+    /**
+     * Shorthand for a Patient's full name
+     */
+    String fullName() {
+        return String.format("%s %s [%s]", firstName, surname, dateOfBirth.toString());
+    }
+}
