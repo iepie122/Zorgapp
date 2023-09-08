53a372779be54b92a74c2caf367a3554cf10c8e5
iepie122 <vanzutphensiep@gmail.com>
2023-09-08 08:55:42 +0200

    Revert "no message"
    This reverts commit ad9e86aac4929fd41fed311b407d120ed6f6ef34.

--- ZorgApp.java
+++ ZorgApp.java
@@ -1,6 +1,6 @@
 class ZorgApp {
     public static void main(String[] args) {
-        User user = new User(1, "Mark ElCamera");
+        User user = new User(1, "Mart ElCamera");
         Administration administration = new Administration(user);
 
         administration.menu();
