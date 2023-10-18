062f5107572efadfce7865e9bdd8be81cecdeb55
Randy Lumunon <jrandyv@gmail.com>
2023-10-18 20:51:51 +0200

    Test Commit 3

--- src/ZorgApp.java
+++ src/ZorgApp.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 class ZorgApp {
     public static void main(String[] args) {
-        User user = new User(1, "Sid Watkins");
+        User user = new User(1, "Mart ElCamera");
         Administration administration = new Administration(user);
 
         administration.menu2();
