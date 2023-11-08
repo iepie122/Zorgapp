118a79767ef644c0926e541d341a3a96b7890c27
moodyroody <144026543+moodyroody@users.noreply.github.com>
2023-11-09 00:57:59 +0100

    Backup commit, no changes

--- src/ZorgApp.java
+++ src/ZorgApp.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 class ZorgApp {
     public static void main(String[] args) {
-        User user = new User(1, "Mart ElCamera");
+        User user = new User(1, "Mart El Camera");
         Administration administration = new Administration(user);
 
         administration.menu2();
